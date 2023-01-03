0b4fada69c5c5d2cc3f388cc58df1a0c3422fb4b
Zoltan Fodor <fodorzoltan317@gmail.com>
2023-01-03 19:31:43 +0100

    Append is null

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 function App() {
   var data = require("./data.json");
 
-  const div = document.createElement("div");
-  div.classList.add("divStyle");
-  const main = document.getElementById("main");
-  main.appendChild(div);
+  for (let i = 0; i < data.length; i++) {
+    const div = document.createElement("div");
+    div.classList.add("divStyle");
+    const main = document.getElementById("main");
+    main.appendChild(div);
+  }
 
   return <div id="main"></div>;
 }

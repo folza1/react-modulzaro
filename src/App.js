b2df2324de1eabb773e25771b54065f95659d893
Zoltan Fodor <fodorzoltan317@gmail.com>
2023-01-03 16:44:26 +0100

    Hooks in for loop

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import "./App.css";
+import React, { useEffect, useRef } from "react";
 
 function App() {
   var data = require("./data.json");
-  for (let i = 0; i < data.length; i++) {
-    const divProduct = document.createElement("div");
-    const mainContainer = document.getElementById("container");
-    divProduct.classList.add("divProduct");
-    mainContainer.appendChild(divProduct);
-  }
-  return <div id="container"></div>;
+
+  //for(let i=0;i<data.length;i++{WHAT COPY HERE TO APPEND THE 30 divProduct?})
+
+  const container = useRef(null);
+
+  const divProduct = document.createElement("div");
+  divProduct.classList.add("divStyle");
+
+  useEffect(() => {
+    container.current.appendChild(divProduct);
+  }, []);
+
+  return (
+    <div>
+      <div id={"container"} ref={container} />
+    </div>
+  );
 }
 export default App;

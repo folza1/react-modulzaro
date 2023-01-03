d6de27b8c94b2ef3db678264181851e7e04eef83
Zoltan Fodor <fodorzoltan317@gmail.com>
2023-01-03 19:28:48 +0100

    Append does not work!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import "./App.css";
-import React, { useEffect, useRef } from "react";
 
 function App() {
   var data = require("./data.json");
 
-  //for(let i=0;i<data.length;i++{WHAT COPY HERE TO APPEND THE 30 divProduct?})
+  const div = document.createElement("div");
+  div.classList.add("divStyle");
+  const main = document.getElementById("main");
+  main.appendChild(div);
 
-  const container = useRef(null);
-
-  const divProduct = document.createElement("div");
-  divProduct.classList.add("divStyle");
-
-  useEffect(() => {
-    container.current.appendChild(divProduct);
-  }, []);
-
-  return (
-    <div>
-      <div id={"container"} ref={container} />
-    </div>
-  );
+  return <div id="main"></div>;
 }
 export default App;

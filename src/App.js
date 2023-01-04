660c4e4b4702d94330d83dd61bbc13d8ab42a7b2
Zoltan Fodor <fodorzoltan317@gmail.com>
2023-01-04 22:17:43 +0100

    Without részletek

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
 import "./App.css";
+import cart from "./cart.jpg";
+import { useState } from "react";
+
+var data = require("./data.json");
 
 function App() {
-  var data = require("./data.json");
+  const [isShown, setIsShown] = useState(false);
 
-  for (let i = 0; i < data.length; i++) {
-    const div = document.createElement("div");
-    div.classList.add("divStyle");
-    const main = document.getElementById("main");
-    main.appendChild(div);
-  }
+  const handleClick = (event) => {
+    setIsShown(true);
+  };
 
-  return <div id="main"></div>;
+  const handleClickClose = (event) => {
+    setIsShown(false);
+  };
+
+  return (
+    <>
+      <h1>Termékek</h1>
+      <div id="main">
+        {data.map((obj) => (
+          <div className="divStyle" key={obj.id}>
+            <div
+              id="thumbNail"
+              style={{ backgroundImage: `url(${obj.thumbnail})` }}
+            ></div>
+            <div id="product">
+              <div id="title">
+                <p>{obj.title}</p>
+              </div>
+              <div id="price">
+                <p>${obj.price}</p>
+              </div>
+              <div
+                id="cart"
+                style={{
+                  backgroundImage: `url(${cart})`,
+                }}
+              ></div>
+            </div>
+            <div id="reszletek">
+              <button id="buttonDetails" onClick={handleClick}>
+                Részletek
+              </button>
+            </div>
+          </div>
+        ))}
+        {isShown && (
+          <div id="window" onClick={handleClickClose}>
+            <div className="details">Részletek</div>
+            <div className="close">Bezárás</div>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 export default App;
